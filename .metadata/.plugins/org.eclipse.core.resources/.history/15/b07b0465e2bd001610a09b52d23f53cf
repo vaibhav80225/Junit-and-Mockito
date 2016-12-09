456da0744e1fe4cef9c38d02543aeaadff02b0cb
vaibhav <vaibhav80225@gmail.com>
2016-12-09 13:27:48 +0530

    updated

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/15/b07b0465e2bd001610a09b52d23f53cf
@@ -0,0 +1,11 @@
+package com.wcisw.junit;
+
+public class GreetingImp implements Greeting {
+
+	@Override
+	public String greet(String name) {
+
+		return "hello " + name;
+	}
+
+}
--- HelloJunit/src/com/wcisw/junit/GreetingImp.java
+++ HelloJunit/src/com/wcisw/junit/GreetingImp.java
@@ -4,7 +4,10 @@ public class GreetingImp implements Greeting {
 
 	@Override
 	public String greet(String name) {
-
+		
+		if(name == null || name.length()== 0){
+			throw new IllegalArgumentException();
+		}
 		return "hello " + name;
 	}
 
